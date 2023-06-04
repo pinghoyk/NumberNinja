f79b2c1b16810c0d9f37617d7d7278a6bebbb85b
Falbue <pinghoyk009@gmail.com>
2023-06-04 17:34:22 +0500

    сделала рандом

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ second_number = 0
 #Создаём окно
 root_Main = Tk() 
 
+#Добавляем изменяемость чисел
+import random
+first_number = random.randint(0, 200)
+second_number = random.randint(0, 200)
+
 #Создаем пример
 example = Label(text = str(first_number) + "+"+ str(second_number) + "=")
 example.pack()
