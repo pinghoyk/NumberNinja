82651addc68f588d5e6017471a6e051ad04014e0
Falbue <pinghoyk009@gmail.com>
2023-06-04 17:40:12 +0500

    создание кнопки

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Добавление импортов
 from tkinter import * 
 import tkinter as tk
+import random
 
 #Переменные
 first_number = 0
@@ -11,12 +12,16 @@ second_number = 0
 root_Main = Tk() 
 
 #Добавляем изменяемость чисел
-import random
 first_number = random.randint(0, 200)
 second_number = random.randint(0, 200)
 
 #Создаем пример
 example = Label(text = str(first_number) + "+"+ str(second_number) + "=")
 example.pack()
+
+
+btn = Button(text='Ответ')
+btn.pack()
+
 
 root_Main.mainloop()
