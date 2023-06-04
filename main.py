39fb2cc67c9188609a3a1186a05ff08afd95c100
Falbue <cyansair05@gmail.com>
2023-06-04 16:32:05 +0500

    Пофиксил Алини коммит

--- main.py
+++ main.py
@@ -2,15 +2,9 @@
 from tkinter import * 
 import tkinter as tk
 
-<<<<<<< HEAD
-root_Main = Tk()
-root_Main.mainloop()
-
-=======
 #Создаём окно
 root_Main = Tk() 
 root_Main.mainloop()
->>>>>>> origin/main
 
 #Создаем пример
 label = Label(text="130-36")
