2be227da935b1e633ed7ffb3fdcafc654cab1609
Falbue <pinghoyk009@gmail.com>
2023-06-06 17:09:38 +0500

    добавила функцию с показом результата и постоянная правильная первая кнопка

--- main.py
+++ main.py
@@ -27,29 +27,38 @@ result = first_number + second_number
 example = Label(text = str(first_number) + "+"+ str(second_number) + "=")
 example.pack()
 
-
-#Переменные для кнопкок
-text_1 = 0
-text_2 = 0
-text_3 = 0
-
-
-#Изменения кнопок
-text_1 = random.randint(0, 200)
-text_2 = random.randint(0, 200)
-text_3 = random.randint(0, 200)
+# Функция, чтобы при нажатии на кнопку мы узнавали ответ
+def check_answer(answer):
+    if int(answer) == result:
+        result_label.config(text="Правильно!")
+    else:
+        result_label.config(text="Неправильно!")
 
 
 
-#Создаем кнопки
-btn1 = Button(text=str(text_1))
+
+
+
+
+#Переменные для кнопкок / Добавление рандома и Постоянный правильный 1 ответ 
+text_1 = str(result)
+text_2 = str(random.randint(0, 300))
+text_3 = str(random.randint(0, 300))
+
+# Создаем кнопки
+btn1 = Button(text=text_1, command=lambda: check_answer(text_1))
 btn1.place(relx=0.3, rely=0.9)
 
-btn2 = Button(text=str(text_2))
+btn2 = Button(text=text_2, command=lambda: check_answer(text_2))
 btn2.place(relx=0.5, rely=0.9)
 
-btn3= Button(text=str(text_3))
+btn3 = Button(text=text_3, command=lambda: check_answer(text_3))
 btn3.place(relx=0.7, rely=0.9)
 
+
+
+#Добавляем строку с отображением результата
+result_label = Label(text="")
+result_label.pack()
 
 root_Main.mainloop()
