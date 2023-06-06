2bf2ab0071ce0fdcb111dcaa4947db69aca78fa4
Falbue <pinghoyk009@gmail.com>
2023-06-06 16:59:29 +0500

    добавление переменных для кнопок

--- main.py
+++ main.py
@@ -20,14 +20,28 @@ second_number = random.randint(0, 200)
 example = Label(text = str(first_number) + "+"+ str(second_number) + "=")
 example.pack()
 
+
+#Переменные для кнопкок
+text_1 = 0
+text_2 = 0
+text_3 = 0
+
+
+#Изменения кнопок
+text_1 = random.randint(0, 200)
+text_2 = random.randint(0, 200)
+text_3 = random.randint(0, 200)
+
+
+
 #Создаем кнопки
-btn1 = Button(text='Ответ')
+btn1 = Button(text=str(text_1))
 btn1.place(relx=0.3, rely=0.9)
 
-btn2 = Button(text='Ответ')
+btn2 = Button(text=str(text_2))
 btn2.place(relx=0.5, rely=0.9)
 
-btn3= Button(text='Ответ')
+btn3= Button(text=str(text_3))
 btn3.place(relx=0.7, rely=0.9)
 
 
